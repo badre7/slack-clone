bf7aec305ad129e6e7e20c8b5f28667453f02b43
badre7 <dieter.dreessen@hotmail.com>
2025-10-31 16:59:46 +0100

    fase inloggen klaar Google werkt niet (1ste keer login gecreeerd)

--- src/features/auth/components/user-button.tsx
+++ src/features/auth/components/user-button.tsx
@@ -1,6 +1,10 @@
 "use client";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useCurrentUser } from "../hooks/user-current-user";
+import { useAuthActions } from "@convex-dev/auth/react";
+
+import { Loader, LogOut } from "lucide-react";
 
 import {
   DropdownMenu,
@@ -9,23 +13,42 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
-
 export const UserButton = () => {
+  const { signOut } = useAuthActions();
+  const { data, isLoading } = useCurrentUser();
+
+  if (isLoading) {
+    return <Loader className="size-4 animate-spin text-muted-foreground" />;
+  }
+
+  if (!data) {
+    return null;
+  }
+
+  const { image, name } = data;
+
+  const avatarFallback = name!.charAt(0).toUpperCase();
+
   return (
     <DropdownMenu modal={false}>
-        <DropdownMenuTrigger className="outline-none relative">
-            <Avatar className="size-10 hover:opacity-75 transition">
-                <AvatarImage />
-                <AvatarFallback>
-
-                </AvatarFallback>
-            </Avatar>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="center" side="right" className="w-60">
-            <DropdownMenuItem>
-
-            </DropdownMenuItem>
-        </DropdownMenuContent>
+      <DropdownMenuTrigger className="outline-none relative">
+        <Avatar className="size-10 hover:opacity-75 transition">
+          <AvatarImage alt={name} src={image} />
+          <AvatarFallback className="bg-sky-500 text-white">
+            {avatarFallback}
+            </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        align="center"
+        side="right"
+        className="w-60 font-bold "
+      >
+        <DropdownMenuItem onClick={() => signOut()} className="h-10">
+          <LogOut className="size-6 mr-2 " />
+          <span className="text-lg font-medium">Log out</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 };
