d661e3fcc6c85da4c85b0042f47d6fd5b9270dbb
badre7 <dieter.dreessen@hotmail.com>
2025-11-06 07:29:46 +0100

    workspaces aan het creeeren bijna op het einde

--- src/features/workspaces/components/create-workspace-modal.tsx
+++ src/features/workspaces/components/create-workspace-modal.tsx
@@ -1,60 +1,63 @@
-"use client"
+"use client";
 
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 import { useCreateWorkspaceModal } from "../store/use-create-workspace-modal";
 import { useCreateWorkspace } from "../api/use-create-workspace";
 
 export const CreateWorkspaceModal = () => {
-    const [open, setOpen] = useCreateWorkspaceModal();
+  const [open, setOpen] = useCreateWorkspaceModal();
+  const [name, setName] = useState("");
 
-    const { mutate } = useCreateWorkspace();
+  const { mutate, isPending } = useCreateWorkspace();
 
-    const handleClose = () => {
-        setOpen(false);
-    };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    const handleSubmit = () =>{
-mutate({
-    name: "Workspace 1",
-}, {
-    onSucces(data) {
-        router.push("/workspaces/${data}")
-    }
-})
-    };
+  const handleSubmit = async (e: React.Formevent<HTMLFormElement>) => {
+    e.preventDefault();
 
-    return (
-        <Dialog open={open} onOpenChange={handleClose}>
-            <DialogContent>
-                <DialogHeader>
-                    <DialogTitle> Add a workspace</DialogTitle>
-                </DialogHeader>
-                <form className="space-y-4">
-                    <Input
-                    value=""
-                    disabled={false}
-                    required
-                    autoFocus
-                    minLength={3}
-                    placeholder="Workspace name e.g. 'Work', 'Personal', 'Home'"
-                    />
-                    <div className="flex justify-end">
-                        <Button disabled={false}>
-                            Create
-                        </Button>
-                    </div>
-
-                </form>
-            </DialogContent>
-        </Dialog>
+    mutate(
+      { name },
+      {
+        onSuccess(data) {
+          console.log(data);
+        },
+      }
     );
-}
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={handleClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle> Add a workspace</DialogTitle>
+        </DialogHeader>
+        <form className="space-y-4">
+          <Input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            disabled={isPending}
+            required
+            autoFocus
+            minLength={3}
+            placeholder="Workspace name e.g. 'Work', 'Personal', 'Home'"
+          />
+          <div className="flex justify-end">
+            <Button disabled={isPending}>Create</Button>
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
