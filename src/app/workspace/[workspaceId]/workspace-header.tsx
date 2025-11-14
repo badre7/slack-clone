1d8dfdd2f495457055f89a178357b1dea3a08081
badre7 <dieter.dreessen@hotmail.com>
2025-11-14 08:26:30 +0100

    halverwege invite modal en soms een hydration error

--- src/app/workspace/[workspaceId]/workspace-header.tsx
+++ src/app/workspace/[workspaceId]/workspace-header.tsx
@@ -12,6 +12,7 @@ import {
 import { Doc } from "../../../../convex/_generated/dataModel";
 import { ChevronDown, ListFilter, SquarePen } from "lucide-react";
 import { PreferencesModal } from "./preferences-modal";
+import { InviteModal } from "./invite-modal";
 
 interface WorkspaceHeaderProps {
   workspace: Doc<"workspaces">;
@@ -20,8 +21,16 @@ interface WorkspaceHeaderProps {
 
 export const WorkspaceHeader = ({ workspace, isAdmin }: WorkspaceHeaderProps) => {
   const [preferencesOpen, setPreferencesOpen] = useState(false);
+  const [inviteOpen, setInviteOpen] = useState(false);
+
   return (
     <>
+    <InviteModal
+    open ={inviteOpen}
+    setOpen={setInviteOpen}
+    name={workspace.name}
+    joinCode={workspace.joinCode}
+    />
     <PreferencesModal
     open={preferencesOpen} 
     setOpen={setPreferencesOpen} 
@@ -54,7 +63,7 @@ export const WorkspaceHeader = ({ workspace, isAdmin }: WorkspaceHeaderProps) =>
             <DropdownMenuSeparator />
           <DropdownMenuItem
           className="cursor-pointer py-2"
-          onClick={() => {}}
+          onClick={() => setInviteOpen(true)}
           >
                 Invite people to { workspace.name}
           </DropdownMenuItem>
