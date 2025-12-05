cba58044b97b11beffaa873b076e8162194758b1
badre7 <dieter.dreessen@hotmail.com>
2025-12-05 21:28:21 +0100

    messages list component 33 min ver

--- src/components/message-list.tsx
+++ src/components/message-list.tsx
@@ -1,5 +1,6 @@
 import { GetMessagesReturnType } from "@/features/messages/api/use-get-messages";
 import { format, isToday, isYesterday } from "date-fns";
+import { Message } from "./message";
 
 interface MessageListProps {
   memberName?: string;
