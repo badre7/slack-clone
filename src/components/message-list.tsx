a4fc6232ca2da1c79d27462f0ec3f1aa43d5c4dc
badre7 <dieter.dreessen@hotmail.com>
2025-12-06 06:31:50 +0100

    messages list component klaar

--- src/components/message-list.tsx
+++ src/components/message-list.tsx
@@ -1,6 +1,8 @@
 import { GetMessagesReturnType } from "@/features/messages/api/use-get-messages";
-import { format, isToday, isYesterday } from "date-fns";
+import { differenceInMinutes, format, isToday, isYesterday } from "date-fns";
 import { Message } from "./message";
+
+const TIME_THRESHOLD = 5;
 
 interface MessageListProps {
   memberName?: string;
@@ -55,6 +57,16 @@ export const MessageList = ({
             </span>
           </div>
           {messages.map((message, index) => {
+            const prevMessage = messages[index-1];
+            const isCompact =
+            prevMessage &&
+            prevMessage.user?._id=== message.user?._id&&
+            differenceInMinutes(
+              new Date(message._creationTime),
+              new Date(prevMessage._creationTime)
+             ) < TIME_THRESHOLD;
+
+
             return (
                 <Message
                 key={message.id}
@@ -70,8 +82,8 @@ export const MessageList = ({
                 createdAt={message._creationTime}
                 isEditing={false}
                 setEditing={() => {}}
-                isCompact={false}
-                hideThreadButton={false}
+                isCompact={isCompact}
+                hideThreadButton={true}
                 threadCount={message.threadcount}
                 threadImage={message.threadImage}
                 threadTimestamp={message.threadTimestamp}
