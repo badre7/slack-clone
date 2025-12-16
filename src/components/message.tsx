d32decbb90bd3e170a9a78317e1727ec197ac4f8
badre7 <dieter.dreessen@hotmail.com>
2025-12-16 01:24:04 +0100

    member profile panel klaar

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -72,7 +72,7 @@ export const Message = ({
   threadName,
   threadTimestamp,
 }: MessageProps) => {
-const { parentMessageId, onOpenMessage, onClose } = usePanel();
+const { parentMessageId, onOpenMessage, onOpenProfile, onClose } = usePanel();
 
   const [ConfirmDialog, confirm] = useConfirm(
     "Delete message",
@@ -87,7 +87,7 @@ const { parentMessageId, onOpenMessage, onClose } = usePanel();
 
   const { mutate: toggleReaction, isPending: isTogglingReaction } = useToggleReaction();
 
-  const isPending = isUpdatingMessage;
+  const isPending = isUpdatingMessage || isTogglingReaction;
 
   const handleReaction = (value: string) => {
     toggleReaction({ messageId: id, value }, {
@@ -214,7 +214,7 @@ const { parentMessageId, onOpenMessage, onClose } = usePanel();
         )}
       >
         <div className="flex items-start gap-2">
-          <button>
+          <button onClick={() => onOpenProfile(memberId)}>
             <Avatar className="h-10 w-10 rounded-md ">
               <AvatarImage className="h-10 w-10 rounded-md" src={authorImage} />
               <AvatarFallback className="h-10 w-10 rounded-md bg-sky-500 text-white text-xs">
@@ -236,7 +236,7 @@ const { parentMessageId, onOpenMessage, onClose } = usePanel();
             <div className="flex flex-col w-coll overflow-hidden">
               <div className="text-sm">
                 <button
-                  onClick={() => {}}
+                  onClick={() => onOpenProfile(memberId)}
                   className="font-bold text-primary hover:underline"
                 >
                   {authorName}
